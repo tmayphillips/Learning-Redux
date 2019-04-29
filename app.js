97c682ebee33b43664170888c943b801aa99946a
TIFFANY MAY-PHILLIPS <tiffanymay-phillips@TIFFANYs-MacBook-Pro.local>
2019-04-29 10:14:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const redux = require('redux')
+
+const initialState = {
+  counter: 0,
+  cartItems: [],
+  isAuthenticated: false
+}
+
+// create a reducer (the only one that can update the store)
+let rootReducer = (state=initialState,action) => {
+  if(action.type == "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    }
+  }
+
+  else if(action.type == "IS_AUTH") {
+    return {
+      ...state,
+      isAuthenticated: action.value
+    }
+  }
+
+  else if(action.type == 'INCREMENT') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
+  return state
+}
+
+// create a store
+const store = redux.createStore(rootReducer)
+console.log(store.getState()) // get global getState
+
+// create subscriptions
+store.subscribe(() => {
+  console.log("[Subscribed]",store.getState());
+})
+
+// dispatching actions
+store.dispatch({
+  type: 'INC_COUNTER'
+})
+
+store.dispatch({
+  type: 'IS_AUTH',value: true // value can be called anything, can pass another object if I want
+})
+
+store.dispatch({
+  type: 'INCREMENT',
+  value: 10
+})
+
+
+console.log(store.getState())
